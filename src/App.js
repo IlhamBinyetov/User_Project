0123c6558417532868cbc81267d59a204b8b66cf
IlhamBinyetov <binyetov631@gmail.com>
2023-04-13 17:44:41 +0400

    Added new Components(Card, Button) and Css Files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from 'react';
 
+import AddUser from './components/Users/AddUser';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <AddUser/>
     </div>
   );
 }
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,11 +1,26 @@
 import React from "react";
+import Card from "../UI/Card";
 
+import styles from './AddUser.module.css'
+import Button from "../UI/Button";
 
-const AddUser = () =>{
+const AddUser = (props) =>{
+
+const addUserhandler = (event) =>{
+    event.preventDefault();
+}
+
     return(
-        <div>
-
-        </div>
+        <Card className={styles.input }>
+            <form onSubmit={addUserhandler}>
+                    <label htmlFor="username">UserName</label>
+                    <input id="usernme" type="text" />
+                    <label htmlFor="age">Age (Years)</label>
+                    <input id="age" type="number" />
+                    <Button type="submit">Add User</Button>
+                </form>
+        </Card>
+  
     )
 }
 
