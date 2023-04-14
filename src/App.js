f61759032f94553bbffc410ea335edec20a68149
IlhamBinyetov <binyetov631@gmail.com>
2023-04-14 10:42:07 +0400

    Added ErrorModal,Userslist components and using useState Hook in many places

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import AddUser from './components/Users/AddUser';
 import './App.css';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+const [usersList, setUserslist] = useState([]);
+
+const addUserhandler = (uName,uAge) =>{
+  setUserslist((prevUsersList) => {
+    return[...prevUsersList,{name:uName, age:uAge, id:Math.random().toString()},]
+  })
+}
+
   return (
     <div className="App">
-      <AddUser/>
+      <AddUser onAddUser={addUserhandler}/>
+      <UsersList users={usersList}/>
     </div>
   );
 }
